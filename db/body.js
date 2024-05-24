04449e660a284b717d3e5fc79c0b303ae5b75757
sohilsk9340 <sohilkhansk942@gmail.com>
2024-05-24 16:44:58 +0530

    Api

--- db/body.js
+++ db/body.js
@@ -1,5 +1,5 @@
 const{ default: mongoose } =require("mongoose");
-const register = require("/.register");
+const register = require("./register");
 const DataBase_Name = process.env.DataBase_Name||"vehicle";
 
 const URI = process.env.Mongo||"mongodb+srv://vehicle:vehicle@cluster0.elwmgmk.mongodb.net"

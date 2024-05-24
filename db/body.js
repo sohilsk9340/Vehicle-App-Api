ea814520de86ee4217a4eab6d1fd7a20ad35c049
sohilsk9340 <sohilkhansk942@gmail.com>
2024-05-24 16:56:16 +0530

    update

--- db/body.js
+++ db/body.js
@@ -1,9 +1,9 @@
 const{ default: mongoose } =require("mongoose");
 const register = require("./register");
-const DataBase_Name = process.env.DataBase_Name||"vehicle";
+const DataBase_Name = process.env.Database_Name||"vehicle";
 
 const URI = process.env.Mongo||"mongodb+srv://vehicle:vehicle@cluster0.elwmgmk.mongodb.net"
-const Mongo = `${URI}/${DataBase_Name}`;
+const Mongo = `${URI}/${Database_Name}`;
 
 mongoose.connect(Mongo).then(() => console.log("connected")).catch((err) => console.log(err))
 
